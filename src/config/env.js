7a32ea2934927f3f2a0cd5bbe2dbbb936f3c261b
Felipelo94 <felipelo298@gmail.com>
2021-11-16 10:33:56 -0500

    Cambio ID fb App

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 let env = {
     baseUrl: "https://dummyapi.io/data/v1",
     appId: "618ed76c43a40935f927f2a7",
-    fbAppId: "254435336569005"
+    fbAppId: "2989786517902439"
 }
 
 export default env;
